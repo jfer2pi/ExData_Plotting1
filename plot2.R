74d388cbe3f27194d1da616698a6260800e61b2c
Fernando Rodriguez Alvarado <fernandorodrig@gmail.com>
2017-10-13 14:55:35 -0500

    Updated with comments

--- plot2.R
+++ plot2.R
@@ -1,27 +1,37 @@
+# Load libraries
+
 library(tidyr)
 library(lubridate)
-setwd("/Users/fer/Dropbox/Coursera DS/Exploratory Data Analysis")
+
+# Assign file url and filenames to variables
 
 url1 <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 file1 <- "datafile.zip"
+
+# If file with filename exists, don't download, it's huge! If it's not there, then download, and unzip
 
 if(!file.exists(file1)) {
     dataset <- download.file(url1, file1, method = "curl")
     unzip(file1, exdir = ".")
 }
 
+# Read table into R, assigning "?" to NA's
 
 testing <- read.table("household_power_consumption.txt", 
                       na.strings = "?", sep = ";",
                       header = TRUE)
 
+# Checks if Date from reading table came in with Date class, if not, uses lubridate to convert
+
 if(class(testing$Date) != "Date") {
     testing$Date <- dmy(testing$Date)
 }
 
+# Subsets the data for only two days in question, then transforms time data with lubridate, then adds dates and times to create
+# a datetime object
 testing.sub <- subset(testing, testing$Date == "2007-02-01" | testing$Date == "2007-02-02")
 testing.sub$Time <- hms(testing.sub$Time)
-testing.sub$Date.Time <- testing.sub$Date + testing.sub$Time
+testing.sub$Date.Time <- testing.sub$Date + testing.sub$
 
 png(filename = "plot2.png", 
     bg = "white", 
